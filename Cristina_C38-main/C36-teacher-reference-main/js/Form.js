39e57247b8b503fc459ed500aff75a784ff2f788
christinawondwessen <78184033+christinawondwessen@users.noreply.github.com>
2021-04-15 20:07:34 -0400

    Add files via upload

---
+++ Cristina_C38-main/C36-teacher-reference-main/js/Form.js
@@ -0,0 +1,40 @@
+class Form {
+  constructor() {
+   this.input=createInput("Name");
+   this.button=createButton('Play');
+   this.greeting=createElement('h2');
+   this.title = createElement('h2');
+  }
+
+hide(){
+  this.greeting.hide();
+  this.button.hide();
+  this.input.hide();
+  this.title.hide();
+}
+
+  display(){
+    title.html("Car Racing Game");
+    this.title.position(displayWidth/2 - 50, 0); 
+    this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80); 
+    this.button.position(displayWidth/2 + 30, displayHeight/2);
+
+    this.button.mousePressed(()=>{
+      this.input.hide();
+      this.button.hide();
+
+      player.name = this.input.value();
+// Addition - major 4
+      playerCount+=1;
+      player.index=playerCount;  
+      player.update();
+      player.updateCount(playerCount);
+      
+      //player.update(name)
+      
+      //var greeting = createElement('h3');
+      this.greeting.html("Hello " + player.name)
+      this.greeting.position(displayWidth/2 - 70, displayHeight/4);
+    });
+  }
+}
